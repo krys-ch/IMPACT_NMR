71d075b55e7f44b2f05507372605ca8552d4add2
krystof_chrappova <76857765+krys-ch@users.noreply.github.com>
2022-09-03 23:36:20 +0100

    Create IMPACT_predict_C.py

---
+++ python_scripts/IMPACT_predict_C.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+tensor_ll = pd.read_csv(r'...')
+zeros = []
+
+for element in np.arange((len(tensor_ll.columns)-12), 80, 1).tolist():
+    a = str(element)
+    zeros.append(a)
+tensor_ll[zeros] = 0
+
+features = np.arange(0, 80, 1).tolist()
+features.append('shift')
+f = []
+for x in features:
+    a = str(x)
+    f.append(a)
+labels = ['shift']
+
+model = pickle.load(open("IMPACT_C.pkl", "rb"))
+tensor_hl = np.array(model.predict(tensor_ll[f]).tolist())
+
+tensor_ll['shift_low_level'] = (tensor_ll['shift'] -205.3726)/-0.9016
+tensor_ll['IMPACT_shift'] = (tensor_hl -187.136)/-1.0399
+
+result_df = tensor_ll[['molecule_name', 'atom_index', 'typeint', 'conn', 'x', 'y', 'z','shift_low_level', 'IMPACT_shift']]
+result_df.to_csv('IMPACT_prediction_CHESHIRE_test.csv')
